d466d4de4fcf91904e2c46c86a069d46013322f8
Tahamidul Haque <tahmidulanon@gmail.com>
2023-11-22 23:35:59 +0600

    project-tools-setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
+});
+console.log(process.cwd());
+
+export default app;
